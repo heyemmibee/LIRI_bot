bfb4497b611c6086d60b8ee49d0e303081bf66ae
eburnaman <eburnaman@gmail.com>
2019-05-20 18:34:58 -0400

    added some keys and spotify

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-//there's a chance I won't get these
+//secretstuff
 
 exports.twitterKeys = {
     consumer_key:'',
     consumer_secret:'',
     access_token_key:'',
     access_token_secret:'',
+},
+
+exports.spotifyKeys = {
+    id: "45aa81a52041460da816a83f9398c244",
+    secret: "5aa96050e37e471ba6e9c07b2189b9a4"
 }
--- liri.js
+++ liri.js
@@ -1,11 +1,10 @@
 const keys = require('./index.js');
 const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 
-const getMyTweets = function() {
-
-    const client = new Twitter(keys.twitterKeys);
-
-    const params = {screen_name: 'heyemmibee'};
+var getMyTweets = function() {
+    var client = new Twitter(keys.twitterKeys);
+    var params = {screen_name: 'heyemmibee'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
      if (!error) {
     // console.log(tweets);
@@ -13,15 +12,58 @@ const getMyTweets = function() {
         console.log(tweets[i].created_at);
         console.log('  ');
         console.log(tweets[i].text);
-        console.log(tweets[i].screen_name);
-        console.log(tweets[i].location);
+        // console.log(tweets[i].screen_name);
+        // console.log(tweets[i].location);
     }
   }
 });
 }
-const pick = function(caseDarta, functionData) {
+
+// Spotify has been updated to require access tokens, needed to switch to new spotify module.
+//     spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+//     if ( err ) {
+//         console.log('Error occurred: ' + err);
+//         return;
+//     }
+//  console.log(data);
+// });
+
+var getMeSpotify = function (songName) {
+    //keeping secret keys secret!
+    var spotify = new Spotify(keys.spotifyKeys);
+  spotify.search({ type: 'track', query: songName}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+//   console.log(data.tracks.items[0]);
+    var songs = data.tracks.items;
+    for (var i=0; i<songs.length; i++) {
+        console.log(i);
+        console.log("Track Name: " + songs[i].name);
+        console.log("Album Name: " + songs[i].album.name);
+        //artist is coming back undefined... something with [object Object],[object Object]
+        // console.log("Artist(s): " + songs[i].artists.name);
+        console.log("Release Date: " + songs[i].album.release_date);
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        
+    }
+
+  });
+}
+
+var pick = function(caseData, functionData) { 
     switch(caseData) {
         case 'my-tweets' :
             getMyTweets();
+        break;
+        case 'spotify-this-song' :
+            getMeSpotify(functionData);
+            default:
+                console.log("Liri won't do that.");
     }
 }
+var runThis = function(arOne, argTwo) {
+    pick(arOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3])
